b5101b71e536db1338966ecb447141b45b9b5ca0
Carla Sampaio <carla@setima.pt>
2019-11-25 12:20:55 +0000

    chore: enabling hive routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,6 +6,7 @@ import InfoRoutes from './../data/info/info.routes'
 import TaskRoutes from './../data/task/task.routes'
 import UserRoutes from './../data/user/user.routes'
 import ApiaryRoutes from './../data/apiary/apiary.routes'
+import HivesRoutes from './../data/hive/hive.routes'
 import CompanyRoutes from './../data/company/company.routes'
 import TaskTypesRoutes from './../data/task_type/task_type.routes'
 import EquipmentsWithIdentificationRoutes from '../data/equipment_with_identification/equipment_with_identification.routes'
@@ -18,6 +19,7 @@ const router = Router()
 router.use('/info', InfoRoutes)
 router.use('/auth', AuthRoutes)
 router.use('/users', UserRoutes)
+router.use('/hives', HivesRoutes)
 router.use('/apiaries', ApiaryRoutes)
 router.use('/roles', RolesRoutes)
 router.use('/companies', CompanyRoutes)
